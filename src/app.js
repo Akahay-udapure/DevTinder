4ac6e879987541e04314601bf3dacbe90761d50c
Akahay-udapure <akshayudapure5@gmail.com>
2025-01-08 10:01:22 +0530

    order of the  route matter

--- src/app.js
+++ src/app.js
@@ -2,14 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello World from server");
-});
-
-app.use("/hello", (req, res) => {
+app.get("/user", (req, res) => {
     res.send("hello hello ");
 });
 
+//this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
     res.send("test hello ");
 });

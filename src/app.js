ab053a6d93aee736d95e05b0d3eb64f32d8501c4
Akahay-udapure <akshayudapure5@gmail.com>
2025-01-08 10:13:46 +0530

    order of the  route matter

--- src/app.js
+++ src/app.js
@@ -2,16 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name", (req, res) => {
     res.send("hello hello ");
 });
-
-//this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-    res.send("test hello ");
-});
-
-// here the  order of the routes matters
 
 app.listen(4500, () => {
     console.log("Server is running on port 4500");

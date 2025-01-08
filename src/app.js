e20e13e119e4646ee0d2cd10142e0daee802c24f
Akahay-udapure <akshayudapure5@gmail.com>
2025-01-08 09:35:21 +0530

    order of the  route matter

--- src/app.js
+++ src/app.js
@@ -3,8 +3,18 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-    console.log("Hello World from server");
+    res.send("Hello World from server");
 });
+
+app.use("/hello", (req, res) => {
+    res.send("hello hello ");
+});
+
+app.use("/test", (req, res) => {
+    res.send("test hello ");
+});
+
+// here the  order of the routes matters
 
 app.listen(4500, () => {
     console.log("Server is running on port 4500");

195bb104c6a5095a454e1634889cbf00194bfbd2
Akahay-udapure <akshayudapure5@gmail.com>
2025-01-09 08:43:23 +0530

    play with multiple request handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name", (req, res) => {
-    res.send("hello hello ");
-});
+app.use(
+    "/user",
+    (req, res, next) => {
+        console.log("response1 log");
+        // res.send("Response1");
+        next();
+    },
+    (req, res, next) => {
+        console.log("response2 log");
+        // res.send("Response2");
+        next();
+    },
+    (req, res, next) => {
+        console.log("response3 log");
+        // res.send("Response3");
+        next();
+    },
+    (req, res, next) => {
+        console.log("response4 log");
+        res.send("Response4");
+        // next();
+    },
+);
 
 app.listen(4500, () => {
     console.log("Server is running on port 4500");

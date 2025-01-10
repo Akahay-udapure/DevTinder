9580face2ed9a280a7e2b755b68bc4fad0f13100
Akahay-udapure <akshayudapure5@gmail.com>
2025-01-10 09:39:45 +0530

    create database connectivity and add user

--- src/app.js
+++ src/app.js
@@ -1,22 +1,30 @@
 const express = require("express");
-const { userAuth, adminAuth } = require("./middlewares/auth");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
 
-app.get("/user/data", userAuth, (req, res) => {
-    res.send("User Data send");
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "Akshay",
+        lastName: "Udapure",
+        emailId: "akshay@gmail.com",
+        password: "aws@123",
+        age: 26,
+        gender: "Male",
+    });
+
+    await user.save();
+    res.status(200).send("User registred successfully");
 });
 
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req, res) => {
-    res.send("Get All Admin Data");
-});
-
-app.get("/admin/deleteAdmin", (req, res) => {
-    res.send("delete admin data");
-});
-
-app.listen(4500, () => {
-    console.log("Server is running on port 4500");
-});
+connectDB()
+    .then(() => {
+        console.log("Connection established successfully....");
+        app.listen(4500, () => {
+            console.log("Server is running on port 4500");
+        });
+    })
+    .catch(() => {
+        console.log("Connection cannot connected....");
+    });

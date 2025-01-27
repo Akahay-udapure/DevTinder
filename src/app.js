4bc76ea668dcf0a21849fcbbc5f858abf38d86af
Akahay-udapure <akshayudapure5@gmail.com>
2025-01-27 22:21:11 +0530

    all crud operation api

--- src/app.js
+++ src/app.js
@@ -16,6 +16,65 @@ app.post("/signup", async (req, res) => {
     }
 });
 
+app.get("/getAllUser", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+});
+
+app.get("/getUserByEmail", async (req, res) => {
+    try {
+        const user = await User.findOne({ emailId: req.body.emailId });
+        res.send(user);
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+});
+
+app.delete("/deleteUser", async (req, res) => {
+    try {
+        await User.findByIdAndDelete({ _id: req.body.userId });
+        res.send("User Deleted Successfully");
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+});
+
+app.patch("/updateUserById", async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            { _id: req.body.userId },
+            req.body,
+            {
+                returnDocument: "after",
+            },
+        ); // here by default options for returnDocument is before
+        console.log(user, "--");
+        res.send("User Updated Successfully");
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+});
+
+app.patch("/updateUserByEmail", async (req, res) => {
+    try {
+        const user = await User.findOneAndUpdate(
+            { emailId: req.body.emailId },
+            req.body,
+            {
+                returnDocument: "after",
+            },
+        ); // here by default options for returnDocument is before
+        console.log(user, "--");
+        res.send("User Updated Successfully");
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+});
+
 connectDB()
     .then(() => {
         console.log("Connection established successfully....");
